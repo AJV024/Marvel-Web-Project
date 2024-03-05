6fa7c4e7764e91ff8ff8b933b238b60e12eb41e4
AJV024 <158377734+AJV024@users.noreply.github.com>
2024-03-04 21:23:51 -0500

    Adjustments leading to Mile 1

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the link elements
+ 
     var homeLink = document.querySelector(".home-link");
     var comicBookLink = document.querySelector(".comicbook-link");
     var cinematicPoliticsLink = document.querySelector(".cinematicpolitics-link");
 
-    // Apply the blue hover text effect
     applyHoverEffect(homeLink);
     applyHoverEffect(comicBookLink);
     applyHoverEffect(cinematicPoliticsLink);
