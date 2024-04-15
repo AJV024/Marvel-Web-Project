cb01fb59529da5ce57e7969ffb4d289c0a878695
AJV024 <158377734+AJV024@users.noreply.github.com>
2024-04-14 23:15:07 -0400

    Trying

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
-var http = require('http')
-var fs = require('fs')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-const PORT = 3000
-fs.readFile('./index.html', function(error, html));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const users = [];
+
+app.get('/', (req, res) => {
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+            res.status(500).send('Error reading index.html');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }
+    });
+});
+
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username;
+    res.send(`Username is ${username}`);
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = {
+            email: req.body.email,
+            password: hashedPassword
+        };
+        users.push(user);
+        res.redirect('/');
+    } catch {
+        res.status(500).send('Error registering user');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
